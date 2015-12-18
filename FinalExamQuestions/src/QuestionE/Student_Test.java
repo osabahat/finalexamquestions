edb1f37be4a76f4cba6d36919a073f39afea20d6
osabahat <osabahat@Osman.fios-router.home>
2015-12-18 17:59:29 -0500

    added #5

--- FinalExamQuestions/src/QuestionE/Student_Test.java
+++ FinalExamQuestions/src/QuestionE/Student_Test.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -40,9 +41,20 @@ public class Student_Test {
 			e.printStackTrace();
 		}
 		Student stu1 = new Student("Bert", "Randall", "Gibbons", dBirthDate, "214 Labrador Lane", "302-893-6889", "bgibbons@udel.edu","MIS");
+		Student stu2 = new Student("John", "Jacob", "Something", dBirthDate, "123 Dalmation Lane", "302-897-4235", "jsomething@udel.edu","MIS");
+		Student stu3 = new Student("Very", "Crazy", "Dude", dBirthDate, "456 Retriever Lane", "302-756-234", "vdude@udel.edu","MIS");
+		Student stu4 = new Student("Really", "Funny", "Girl", dBirthDate, "789 Chihuahua Lane", "302-542-3462", "rgirl@udel.edu","MIS");
+		Student stu5 = new Student("Kinda", "Boring", "Guy", dBirthDate, "101 Dachsund Lane", "302-744-2432", "kguy@udel.edu","MIS");
 
-
-		assertTrue(1==1);
+		HashMap students = new HashMap(); 
+		
+		students.put(stu1, stu1.getPersonID());
+		students.put(stu2, stu2.getPersonID());
+		students.put(stu3, stu3.getPersonID());
+		students.put(stu4, stu4.getPersonID());
+		students.put(stu5, stu5.getPersonID());
+		
+		assertTrue(students.size() == 5);
 	}
 
 }
